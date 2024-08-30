88d4785f095b8129eb3f7a6dfa79cae8872e2a7e
dcodner24 <dccodner24@gmail.com>
2024-08-29 21:36:46 -0500

    Updated ENV vars

--- 17.0/entrypoint.sh
+++ 17.0/entrypoint.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-if [ -v PASSWORD_FILE ]; then
-    PASSWORD="$(< $PASSWORD_FILE)"
+if [ -v POSTGRES_PASSWORD_FILE ]; then
+    PASSWORD="$(< $POSTGRES_PASSWORD_FILE)"
 fi
 
-# set the postgres database host, port, user and password according to the environment
+# set the postgres database host, port, user, and password according to the environment
 # and pass them as arguments to the odoo process if not present in the config file
-: ${HOST:=${DB_PORT_5432_TCP_ADDR:='db'}}
-: ${PORT:=${DB_PORT_5432_TCP_PORT:=5432}}
-: ${USER:=${DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='odoo'}}}
-: ${PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='odoo'}}}
+: ${POSTGRES_HOST:=${POSTGRES_DB_PORT_5432_TCP_ADDR:='db'}}
+: ${POSTGRES_PORT:=${POSTGRES_DB_PORT_5432_TCP_PORT:=5432}}
+: ${POSTGRES_USER:=${POSTGRES_DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='odoo'}}}
+: ${POSTGRES_PASSWORD:=${POSTGRES_DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='odoo'}}}
 
 DB_ARGS=()
 function check_config() {
@@ -23,10 +23,10 @@ function check_config() {
     DB_ARGS+=("--${param}")
     DB_ARGS+=("${value}")
 }
-check_config "db_host" "$HOST"
-check_config "db_port" "$PORT"
-check_config "db_user" "$USER"
-check_config "db_password" "$PASSWORD"
+check_config "db_host" "$POSTGRES_HOST"
+check_config "db_port" "$POSTGRES_PORT"
+check_config "db_user" "$POSTGRES_USER"
+check_config "db_password" "$POSTGRES_PASSWORD"
 
 case "$1" in
     -- | odoo)
