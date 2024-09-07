7f68f269fc7b9a3ea3bad7735dc134976121c80f
David Codner <dccodner24@gmail.com>
2024-09-07 11:54:28 -0500

    Updated Config and Controller

--- 17.0/custom-modules/https_override/controllers/routes.py
+++ 17.0/custom-modules/https_override/controllers/routes.py
@@ -3,11 +3,9 @@ from odoo.http import request
 
 class HTTPSUpgrade(http.Controller):
 
-    @http.route('/<path:path>', type='http', auth="public", website=True)
-    def catch_all(self, path, **kwargs):
+    @http.route('/', type='http', auth='public', website=True)
+    @http.route('/<path:path>', type='http', auth='public', website=True)
+    def catch_all(self, path=None, **kwargs):
         response = request.render(path, **kwargs)
-
-        # Set the Content-Security-Policy header
         response.headers['Content-Security-Policy'] = "upgrade-insecure-requests"
-
         return response
